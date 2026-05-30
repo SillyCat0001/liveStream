2bc649abebcda52f40f36f8afde4f6293e5bf528
Developer <dev@example.com>
2026-05-30 21:50:08 +0800

    feat(stream-server): add Redis keyspace notification listener for heartbeat expiry

---
+++ liveStream/stream-server/src/main/java/cn/livestream/server/config/RedisKeyExpirationListener.java
@@ -0,0 +1,24 @@
+package cn.livestream.server.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+
+public class RedisKeyExpirationListener implements MessageListener {
+
+    private static final Logger log = LoggerFactory.getLogger(RedisKeyExpirationListener.class);
+    private static final String HEARTBEAT_KEY_PREFIX = "STREAM:HEARTBEAT:";
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        String expiredKey = message.toString();
+        log.debug("Redis key expired: {}", expiredKey);
+
+        if (expiredKey.startsWith(HEARTBEAT_KEY_PREFIX)) {
+            String agentId = expiredKey.substring(HEARTBEAT_KEY_PREFIX.length());
+            log.info("Heartbeat expired for agent: {}", agentId);
+            // StreamCoordinator.onHeartbeatExpired(agentId) will be called in Task 3
+        }
+    }
+}
