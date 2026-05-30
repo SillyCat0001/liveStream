82cff37f53f8ac902af82df15be95a51d3fbdeb4
Developer <dev@example.com>
2026-05-30 21:52:08 +0800

    fix: add missing @Component on RedisKeyExpirationListener

--- liveStream/stream-server/src/main/java/cn/livestream/server/config/RedisKeyExpirationListener.java
+++ liveStream/stream-server/src/main/java/cn/livestream/server/config/RedisKeyExpirationListener.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RedisKeyExpirationListener implements MessageListener {
 
     private static final Logger log = LoggerFactory.getLogger(RedisKeyExpirationListener.class);
